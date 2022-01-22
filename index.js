ae0061ea0d571eb0cbf16cd085f13cc378695f22
ryan <ryan.williams93@aol.com>
2022-01-21 20:24:48 -0600

    trying to send info to page-template

--- index.js
+++ index.js
@@ -1,151 +1,160 @@
 // The packages needed for this application
 const fs = require('fs');
+
 const inquirer = require('inquirer');
 const generateSite = require('./utils/generateSite.js');
 
 // generates the file
-const generatePage = require('./src/page-template.js');
-const Employee = require('./lib/Employee.js');
 const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+const generateHTML = require('./src/page-template.js');
 
+const teamMembers = [];
 const idArray = [];
 
-function promptManager() {
-    console.log('Welcome to Happy Tappy Inc. Please enter employee information below: ');
-    return inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: 'Team manager name: ',
-        },
-        {
-            type: 'input',
-            name: 'managerId',
-            message: 'Team manager id number: ',
-        },
-        {
-            type: 'input',
-            name: 'managerEmail',
-            message: 'Team manager email:',
-        },
-        {
-            type: 'input',
-            name: 'managerOfficeNumber',
-            message: 'Team manager office number: '
-        }]
-    )
-    .then(answers => { 
-        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
-        teamMembers.push(manager);
-        idArray.push(answers.managerId);
-        buildTeam();
-    })
-};
+function startJob() {
 
-function buildTeam() {
-    return inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'role',
-            message: 'Pick a role:',
-            choices: ['Engineer', 'Intern', 'None']
-        }])
-        .then(choice => {
-            switch (choice.role) {
-                case 'Engineer': 
-                    promptEngineer();
-                    break;
-                case 'Intern': 
-                    promptIntern();
-                    break;
-                case 'None': 
-                    finalizeTeam();
-            }
-        });
-};
+    function promptManager() {
+        console.log('Welcome to Happy Tappy Inc. Please enter Team Manager information below: ');
+        return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: 'Team manager name:',
+            },
+            {
+                type: 'input',
+                name: 'managerId',
+                message: 'Team manager ID number:',
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: 'Team manager email:',
+            },
+            {
+                type: 'input',
+                name: 'managerOfficeNumber',
+                message: 'Team manager office number:'
+            }]
+        )
+        .then(answers => { 
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamMembers.push(manager);
+            idArray.push(answers.managerId);
+            buildTeam();
+        })
+    };
+    
+    function buildTeam() {
+        return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: 'Pick a role:',
+                choices: ['Engineer', 'Intern', 'None']
+            }])
+            .then(choice => {
+                switch (choice.role) {
+                    case 'Engineer': 
+                        promptEngineer();
+                        break;
+                    case 'Intern': 
+                        promptIntern();
+                        break;
+                    default:
+                        finalizeTeam(teamMembers);
+                }
+            });
+    };
+    
+    function promptEngineer() {
+        console.log('Enter Engineer information below:');
+        return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: 'Engineer name:',
+            },
+            {
+                type: 'input',
+                name: 'engineerId',
+                message: 'Engineer ID number:',
+            },
+            {
+                type: 'input',
+                name: 'engineerEmail',
+                message: 'Engineer email:',
+            },
+            {
+                type: 'input',
+                name: 'engineerGithub',
+                message: 'Engineer github username:'
+            },
+        ])
+        .then(answers => { 
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer);
+            idArray.push(answers.engineerId);
+            buildTeam();
+        })
+    };
+    
+    function promptIntern() {
+        console.log('Enter Intern information below:');
+        return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: 'Intern name:',
+            },
+            {
+                type: 'input',
+                name: 'internId',
+                message: 'Intern ID number:',
+            },
+            {
+                type: 'input',
+                name: 'internEmail',
+                message: 'Intern email:',
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "Intern current school:"
+            }]
+        )
+        .then(answers => { 
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            teamMembers.push(intern);
+            idArray.push(answers.internId);
+            buildTeam();
+        })
+    };
+    
+    // function finalizeTeam() {
+    //     teamMembers => {
+    //         return new Promise((response, reject) => {
+    //             fs.writeFileSync('./dist/index.html', generateHTML(teamMembers), err => {
+    //                 if (err) {
+    //                     reject(err);
+    //                     return;
+    //                 }
+    //                 response({
+    //                     ok: true,
+    //                     message: 'File created!'
+    //                 });
+    //             });
+    //         });
+    //     };
+    // }
 
-function promptEngineer() {
-    console.log('Welcome to Happy Tappy Inc. Please enter employee information below: ');
-    return inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'engineerName',
-            message: 'Engineer name: ',
-        },
-        {
-            type: 'input',
-            name: 'engineerId',
-            message: 'Engineer id number: ',
-        },
-        {
-            type: 'input',
-            name: 'engineerEmail',
-            message: 'Engineer email: ',
-        },
-        {
-            type: 'input',
-            name: 'engineerGithub',
-            message: 'Engineer github username: '
-        },
-    ])
-    .then(answers => { 
-        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, );
-        teamMembers.push(engineer);
-        idArray.push(answers.engineerId);
-        buildTeam();
-    })
-};
-function promptManager() {
-    console.log('Welcome to Happy Tappy Inc. Please enter employee information below: ');
-    return inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: 'Team manager name: ',
-        },
-        {
-            type: 'input',
-            name: 'managerId',
-            message: 'Team manager id number: ',
-        },
-        {
-            type: 'input',
-            name: 'managerEmail',
-            message: 'Team manager email:',
-        },
-        {
-            type: 'input',
-            name: 'managerOfficeNumber',
-            message: 'Team manager office number: '
-        }]
-    )
-    .then(answers => { 
-        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
-        teamMembers.push(manager);
-        idArray.push(answers.managerId);
-        buildTeam();
-    })
-};
-
-function promptIntern() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "School",
-            message: "what school do you attend?"
-        },
-        {
-            type: "confirm",
-            name: "addEmployee",
-            message: "Would you like to add another employee?"
-        }
-    ])
+    promptManager()
 }
 
-promptManager();
-
+startJob();
---
+++ src/page-template.js
@@ -0,0 +1,68 @@
+const Engineer = require("../../Employees-R-Us/lib/Engineer");
+const Manager = require("../../Employees-R-Us/lib/Manager");
+const Intern = require("../../Employees-R-Us/lib/Intern");
+const Employee = require("../lib/Employee");
+
+
+// create the about section
+function generateEmployee(teamMembers) {
+    for (let i = 0; i < teamMembers.length; i++) {
+        let teamPage =  `
+            <div class="card display-inline-block">
+                <div class="card-head>
+                    <h2>${teamMembers[i].name}</h2>
+                    <h2>${teamMembers[i].getRole()}</h2>
+                </div>
+                <div class="card-body>
+                    <h4>EmployeeID#: ${teamMembers[i].id}</h4>
+                    <a href="sendto:${teamMembers[i].email}">Email: ${teamMembers[i].email}</a>`
+                if (teamMembers[i].officeNumber) {
+                    teamPage += `
+                    <p>Office Number: ${teamMembers[i].officeNumber}</p>`
+                }
+                if (teamMembers[i].github) {
+                    teamPage += `
+                    <p><a href="http://github.com/${teamMembers[i].github}"></a></p>`
+                } 
+                if (teamMembers[i].school) {
+                    teamPage += `
+                    <p>University: ${teamMembers[i].school}</p>`
+                }
+    }
+  
+};
+
+function generateHTML(teamMembers) {
+    console.log('file groped')
+  // destructure page data by section
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">Happy Tappy Inc.</h1>
+      </div>
+    </header>
+    <main class="container my-5">
+      ${generateEmployee(teamMembers)}
+    </main>
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()}</h3>
+    </footer>
+  </body>
+  </html>
+  `;
+};
+
+module.exports = generateHTML;
